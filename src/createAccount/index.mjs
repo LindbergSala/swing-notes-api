7422b2368e01fbccaff4a394c1086db0329d0dc3
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-09-22 16:02:30 +0200

    middy-Updaye

--- src/createAccount/index.mjs
+++ src/createAccount/index.mjs
@@ -1,9 +1,19 @@
-export const handler = async (event) => {
-    const account = JSON.parse(event.body); // { username, password, email }
+import middy from '@middy/core';
+import jsonBodyParser from '@middy/http-json-body-parser';
+import validator from '@middy/validator';
+import { transpileSchema } from '@middy/validator/transpile';
+import { createAccountSchema } from '../middlewares/schemas/Index.mjs';
 
+const lambdaHandler = async (event) => {
+    const account = event.body; // { username, email, password }
 
     return {
         statusCode: 200,
         body: JSON.stringify({ success: true, account }),
     };
 };
+
+export const handler = middy()
+.use(jsonBodyParser())
+.use(validator({ eventSchema: transpileSchema(createAccountSchema) }))
+.handler(lambdaHandler);
---
+++ src/middlewares/schemas/Index.mjs
@@ -0,0 +1,16 @@
+export const createAccountSchema = {
+    type: 'object',
+    require: ["body"],
+    properties: {
+        body: {
+            type: 'object',
+            required: ['username', 'email', 'password'],
+            properties: {
+                username: { type: 'string', minLength: 3 },
+                email: { type: 'string', format: 'email' },
+                password: { type: 'string', minLength: 8 }
+            },
+        },     
+    },
+};
+
